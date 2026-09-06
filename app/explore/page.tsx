85f1cf852dba139c89c5e076104be81ca48faade
saviourben177-dotcom <saviourben177@gmail.com>
2026-09-06 09:51:16 -0700

    Connect Explore to live Nigerian market price feed

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,9 +1,14 @@
+import { getOpaindexPrices } from "@/lib/opaindex";
 import { getProductSummaries } from "@/lib/queries";
 import ExploreClient from "./explore-client";
 
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
 
 export default async function ExplorePage() {
-  const products = await getProductSummaries();
-  return <ExploreClient products={products} />;
+  const [products, marketPrices] = await Promise.all([
+    getProductSummaries(),
+    getOpaindexPrices(),
+  ]);
+
+  return <ExploreClient products={products} marketPrices={marketPrices} />;
 }
